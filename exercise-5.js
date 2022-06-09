0fa057c4866fc1e74f13cc9421e643893d16cb2c
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-06-09 15:56:10 +0200

    es LETTO

---
+++ exercise-5.js
@@ -0,0 +1,50 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    this.#amount += amount;
+  }
+
+  withdraw(amount) {
+    this.#amount -= amount;
+  }
+
+  view() {
+    console.log(this.#amount);
+  }
+}
+
+class BankAccountVip extends BankAccount {
+  
+constructor(initialAmount){
+  super(initialAmount)
+    
+  }
+
+
+deposit(amount){
+  if (amount >= 1000){
+    super.deposit(amount + ((amount / 100) * 3))
+  }
+  else {
+    super.deposit(amount)
+  }
+}
+
+
+
+}
+
+// questo esercizio era già completato, però l'ho capito
+
+
+const bankAccountVip = new BankAccountVip(1000);
+bankAccountVip.deposit(500);
+bankAccountVip.deposit(1200);
+bankAccountVip.withdraw(800);
+bankAccountVip.deposit(3500);
+bankAccountVip.view();
